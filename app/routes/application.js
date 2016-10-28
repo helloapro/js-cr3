bb7c6a3dbcd30ddeab1db15acbdb66fb894b1427
April Peng <helloaprilpeng@gmail.com>
2016-10-28 09:11:05 -0700

    create question tile and display functionality in homepage

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     click() {
-      $('.ui.labeled.icon.sidebar').sidebar('toggle');
+      Ember.$('.ui.labeled.icon.sidebar').sidebar('toggle');
     }
   }
 });
