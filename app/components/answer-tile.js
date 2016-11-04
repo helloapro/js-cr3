d28218d41fc813cc1c26d74abf7ced99e5c20ec4
April Peng <helloaprilpeng@gmail.com>
2016-11-04 13:57:36 -0700

    start functionality to upvote answers

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  userSession: Ember.inject.service(),
+  actions: {
+    upvoteAnswer(answer) {
+      this.get('userSession').upvoteAnswer(answer);
+    }
+  }
 });
--- app/services/user-session.js
+++ app/services/user-session.js
@@ -1,36 +1,50 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  loggedIn: false,
-  questions: [],
+  // loggedIn: false,
+  // questions: [],
   answers: [],
-  replies: [],
+  // replies: [],
 
-  login() {
-    this.set('loggedIn', true);
-  },
-  logout() {
-    this.set('loggedIn', false);
-  },
+  // login() {
+  //   this.set('loggedIn', true);
+  // },
+  // logout() {
+  //   this.set('loggedIn', false);
+  // },
 
-  includes(question) {
-    return this.get('questions').includes('question');
-  },
-  add(question) {
-    this.get('questions').pushObject(question);
-  },
+  // includes(question) {
+  //   return this.get('questions').includes('question');
+  // },
+  // add(question) {
+  //   this.get('questions').pushObject(question);
+  // },
 
-  includes(answer) {
-    return this.get('answers').includes('answer');
-  },
-  add(answer) {
+  // includes(answer) {
+  //   return this.get('answers').includes('answer');
+  // },
+  // addAnswer(answer) {
+  //   this.get('answers').pushObject(answer);
+  // },
+  downvoteAnswer(answer) {
     this.get('answers').pushObject(answer);
+    answer.set('downvote', answer.get('downvote') + 1);
+    console.log(answer);
   },
-
-  includes(reply) {
-    return this.get('replies').includes('reply');
-  },
-  add(reply) {
-    this.get('replies').pushObject(reply);
+  upvoteAnswer(answer) {
+    this.get('answers').pushObject(answer);
+    answer.set('upvote', answer.get('upvote') + 1);
+    console.log(answer);
   }
+
+  // includes(reply) {
+  //   return this.get('replies').includes('reply');
+  // },
+  // add(reply) {
+  //   this.get('replies').pushObject(reply);
+  // },
+  //
+  // empty() {
+  //   this.get('answers').setObjects([]);
+  // }
 });
