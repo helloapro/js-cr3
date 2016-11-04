2969463233a393d6cc03a8c70d742a31710bf41c
April Peng <helloaprilpeng@gmail.com>
2016-11-04 16:55:51 -0700

    build select sort filter and functionality to change listing order of answers

--- app/components/question-page.js
+++ app/components/question-page.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['date:desc'],
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
+  actions: {
+    updateSort() {
+      var value = this.$('#sortedAnswers').val();
+      this.set('sortBy', [value]);
+    }
+  }
 });
