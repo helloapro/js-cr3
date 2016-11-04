3f64d175c05905bcb38934b4caafd8920c8b7bad
April Peng <helloaprilpeng@gmail.com>
2016-11-04 10:08:11 -0700

    update functionality and styling

--- app/models/answer.js
+++ app/models/answer.js
@@ -4,11 +4,11 @@ export default DS.Model.extend({
   author: DS.attr(),
   answercontent: DS.attr(),
   question: DS.belongsTo('question', { async: true }),
-  upvote: DS.attr(),
-  downvote: DS.attr(),
+  upvote: DS.attr('number'),
+  downvote: DS.attr('number'),
   date: DS.attr('string'),
 
   ranking: Ember.computed('upvote', 'downvote', function() {
-    parseInt(this.get('upvote') - this.get('downvote'));
+    return parseInt(this.get('upvote') - this.get('downvote'));
   }),
 });
--- app/models/question.js
+++ app/models/question.js
@@ -4,12 +4,12 @@ export default DS.Model.extend({
   author: DS.attr(),
   questioncontent: DS.attr(),
   notes: DS.attr(),
-  answers: DS.hasMany('answer', { async: true })
-  upvote: DS.attr(),
-  downvote: DS.attr(),
+  answers: DS.hasMany('answer', { async: true }),
+  upvote: DS.attr('number'),
+  downvote: DS.attr('number'),
   date: DS.attr('string'),
 
   ranking: Ember.computed('upvote', 'downvote', function() {
-    parseInt(this.get('upvote') - this.get('downvote'));
+    return parseInt(this.get('upvote') - this.get('downvote'));
   }),
 });
